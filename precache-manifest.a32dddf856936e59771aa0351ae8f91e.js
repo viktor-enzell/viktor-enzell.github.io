41eaa9e6bec179ee141b389d8a431d047bccd1b5
viktor-enzell <viktor.enzell@gmail.com>
2022-07-12 15:02:36 +0200

    Updates

---
+++ precache-manifest.a32dddf856936e59771aa0351ae8f91e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf6b4eac56087d4fe730986e9da43d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b8a6a8711faa54802c8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c6538573f48f00f557c0",
+    "url": "/static/js/2.5fa32646.chunk.js"
+  },
+  {
+    "revision": "b7e86ad4d38b1a88ab44b6670b221c75",
+    "url": "/static/js/2.5fa32646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b8a6a8711faa54802c8",
+    "url": "/static/js/main.21cdc6e1.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
